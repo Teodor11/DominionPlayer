a0a9e1fef5f6fdf8154db9e54a86acd3f1b6031a
Teodor <24278460+Teodor11@users.noreply.github.com>
2026-01-14 18:58:31 +0100

    Added countStones and countEdgeStones functions

--- Players/ESZTIKplayer/src/main/java/put/ai/games/ESZTIKplayer/ESZTIKplayer.java
+++ Players/ESZTIKplayer/src/main/java/put/ai/games/ESZTIKplayer/ESZTIKplayer.java
@@ -11,6 +11,8 @@ import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
+import static java.lang.Math.sqrt;
+
 public class ESZTIKplayer extends Player {
 
     private Random random = new Random(0xdeadbeef);
@@ -52,10 +54,19 @@ public class ESZTIKplayer extends Player {
     /**
      * Funkcja oblicza aktualną liczbę wszystkich pionków danego koloru na planszy
      */
-    private int calculateStones(Board board, Color color) {
-
-
-        return 0;
+    private int countStones(Board board, Color color) {
+        int count = 0;
+        int size = board.getSize();
+        int edge = (int) Math.sqrt(size);
+        for (int i = 0; i < edge; i++) {
+            for (int j = 0; j < edge; j++) {
+                Color field = board.getState(i, j);
+                if (field == color) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
 
@@ -63,30 +74,44 @@ public class ESZTIKplayer extends Player {
      * Funkcja oblicza aktualną liczbę pionków przy krawędziach / w narożnikach danego koloru na planszy.
      * Pionki przy krawędziach są liczone jako 1, a w narożnikach jako 2.
      */
-    private int calculateEdgeStones(Board board, Color color) {
+    private int countEdgeStones(Board board, Color color) {
+        int count = 0;
+        int size = board.getSize();
+        int edge = (int) Math.sqrt(size);
 
+        for (int i = 0; i < edge; i++) {
+            Color field1 = board.getState(i, 0);
+            Color field2 = board.getState(i, edge-1);
+            Color field3 = board.getState(0, i);
+            Color field4 = board.getState(edge-1, i);
 
-        return 0;
+            if (field1 == color) { count++; }
+            if (field2 == color) { count++; }
+            if (field3 == color) { count++; }
+            if (field4 == color) { count++; }
+        }
+
+        return count;
     }
 
-    private int getscore(Board board, Color color){
-        return calculateEdgeStones(board, color)+calculateStones(board, color);
+    private int getScore(Board board, Color color){
+        return countEdgeStones(board, color) + countStones(board, color);
     }
 
     private int evaluateBoard(Board board, Color playerColor, Color enemyColor) {
-        int score = getscore(board, playerColor);
+        int score = getScore(board, playerColor);
 
         //if(playerColor == getWinner(playerColor)) return Integer.MAX_VALUE;
 
         List<Move> enemyMoves = board.getMovesFor(enemyColor);
         for (Move enemyMove : enemyMoves){
             board.doMove(enemyMove);
-            score -= getscore(board, enemyColor);
+            score -= getScore(board, enemyColor);
             
             List<Move> moves = board.getMovesFor(playerColor);
             for (Move move : moves){
                 board.doMove(move);
-                score += getscore(board, playerColor);
+                score += getScore(board, playerColor);
                 board.undoMove(move);
             }
 
